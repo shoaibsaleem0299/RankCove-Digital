28d74699bb4eda0fd45d367fb43490e7b22f0531
shoaibsaleem0299 <shoaib0299@gmail.com>
2024-09-03 19:05:12 +0500

    resolve the prblem

--- src/app/contactus/page.tsx
+++ src/app/contactus/page.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image'; // Import Image component from Next.js
 const page = () => {
   return (
     <div className='contact-us'>
-      <HeroSection title='Contact Us'/>
+      <HeroSection title='Contact Us' />
       <div className="mx-auto max-w-7xl px-4 h-screen flex items-center">
         <div className="mx-auto max-w-7xl py-12 md:py-24">
           <div className="grid items-center justify-items-center gap-x-4 gap-y-10 lg:grid-cols-2">
@@ -19,24 +19,92 @@ const page = () => {
                   Our friendly team would love to hear from you.
                 </p>
                 <form action="" className="mt-8 space-y-4">
-                  {/* Form fields */}
+                  <div className="grid w-full gap-y-4 md:gap-x-4 lg:grid-cols-2">
+                    <div className="grid w-full  items-center gap-1.5">
+                      <label
+                        className="text-sm font-medium leading-none text-white peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                        htmlFor="first_name"
+                      >
+                        First Name
+                      </label>
+                      <input
+                        className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
+                        type="text"
+                        id="first_name"
+                        placeholder="First Name"
+                      />
+                    </div>
+                    <div className="grid w-full  items-center gap-1.5">
+                      <label
+                        className="text-sm font-medium leading-none text-white peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                        htmlFor="last_name"
+                      >
+                        Last Name
+                      </label>
+                      <input
+                        className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
+                        type="text"
+                        id="last_name"
+                        placeholder="Last Name"
+                      />
+                    </div>
+                  </div>
+                  <div className="grid w-full  items-center gap-1.5">
+                    <label
+                      className="text-sm font-medium leading-none text-white peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                      htmlFor="email"
+                    >
+                      Email
+                    </label>
+                    <input
+                      className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
+                      type="text"
+                      id="email"
+                      placeholder="Email"
+                    />
+                  </div>
+                  <div className="grid w-full  items-center gap-1.5">
+                    <label
+                      className="text-sm font-medium leading-none text-white peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                      htmlFor="phone_number"
+                    >
+                      Phone number
+                    </label>
+                    <input
+                      className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
+                      type="tel"
+                      id="phone_number"
+                      placeholder="Phone number"
+                    />
+                  </div>
+                  <div className="grid w-full  items-center gap-1.5">
+                    <label
+                      className="text-sm font-medium leading-none text-white peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                      htmlFor="message"
+                    >
+                      Message
+                    </label>
+                    <textarea
+                      className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
+                      id="message"
+                      placeholder="Leave us a message"
+
+                    ></textarea>
+                  </div>
+                  <button
+                    type="button"
+                    className="w-full rounded-md bg-[#761FE3] px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+                  >
+                    Send Message
+                  </button>
                 </form>
               </div>
             </div>
-            {/* Updated Image Component */}
-            <div className="hidden lg:block relative max-h-full w-full">
-              <Image
-                alt="Contact us"
-                className="rounded-lg object-cover"
-                src="https://images.unsplash.com/photo-1543269664-56d93c1b41a6?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzZ8fGhhcHB5JTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
-                fill
-                objectFit="cover"
-                objectPosition="center"
-                sizes="(max-width: 768px) 100vw,
-                       (max-width: 1200px) 50vw,
-                       33vw"
-              />
-            </div>
+            <img
+          alt="Contact us"
+          className="hidden max-h-full w-full rounded-lg object-cover lg:block"
+          src="https://images.unsplash.com/photo-1543269664-56d93c1b41a6?ixlib=rb-4.0.3ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzZ8fGhhcHB5JTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
+        />
           </div>
         </div>
       </div>
