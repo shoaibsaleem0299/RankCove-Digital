282e059939e77b9c88b752dd531ab9b4807d5424
shoaibsaleem0299 <shoaib0299@gmail.com>
2024-09-05 13:41:59 +0500

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -105,18 +105,7 @@ const blogs = [
     category: "Tutorial",
     author: "Jese Leos",
     authorImage: "https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png",
-    blog: "In today's digital age, a strong online presence is no longer a luxury but a necessity for businesses, individuals, and organizations alike. It's a platform to connect with audiences, build brand awareness, and drive growth. To effectively establish and maintain a robust online presence, several key strategies must be implemented.  " +
-    "A well-optimized website is the cornerstone of a strong online presence. This involves ensuring it is user-friendly, mobile-responsive, and search engine optimized (SEO). SEO techniques, such as keyword research and on-page optimization, help your website rank higher in search engine results pages (SERPs). Additionally, a fast-loading website improves user experience and can positively impact your search engine rankings.  " +
-    "Consistent production of valuable and engaging content is essential for attracting and retaining an audience. This content can take various forms, including blog posts, articles, videos, infographics, and social media updates. " +
-       "The key is to create content that is relevant, informative, and shares your unique value proposition.  " +
-    "Social media platforms offer a powerful way to connect with your target audience and build relationships. By actively engaging with your followers, sharing valuable content, and participating in relevant conversations, you can increase your brand visibility and drive traffic to your website. It's important to select the right platforms for your audience and develop a consistent social media strategy.  " +
-    "Networking is crucial for building a strong online presence. Connect with other businesses, influencers, and industry experts through online communities, forums, and events. Collaborate on projects, share content, and offer support to build mutually beneficial relationships.  " +
-    "Email marketing remains a highly effective tool for reaching and engaging with your audience. Build an email list of subscribers and send regular, personalized newsletters with valuable content, updates, and promotions. Segment your email list to target specific audience segments with tailored messages.  " +
-    "For businesses with a local focus, local SEO is essential. Optimize your Google My Business listing with accurate information, photos, and reviews. Encourage customers to leave positive reviews and claim your business on other local directories.  " +
-    "To measure the effectiveness of your online presence, use analytics tools to track website traffic, user behavior, and social media engagement. Analyze the data to identify areas for improvement and make data-driven decisions to optimize your strategies.  " +
-    "High-quality visuals, such as images and videos, can enhance your content and make it more engaging. Use visually appealing elements to break up text and capture attention.  " +
-    "The digital landscape is constantly evolving. Stay informed about the latest trends, technologies, and best practices to ensure your online presence remains relevant and effective.  " +
-    "By implementing these strategies, you can build a strong online presence that helps you achieve your business goals and connect with your target audience. Remember, building a successful online presence is an ongoing process that requires consistent effort and adaptation."  },
+  },
   {
     title: "How to Boost Your Website Traffic with These Simple SEO Tips ",
     description: "Nowadays, having a website is essential for businesses and individuals alike. However, simply having a website isn’t enough – you need to ensure that it",
@@ -124,25 +113,19 @@ const blogs = [
     category: "Article",
     author: "Bonnie Green",
     authorImage: "https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png",
-    blog: "In today's digital age, a strong online presence is no longer a luxury but a necessity for businesses, individuals, and organizations alike. It's a platform to connect with audiences, build brand awareness, and drive growth. To effectively establish and maintain a robust online presence, several key strategies must be implemented.  " +
-      "A well-optimized website is the cornerstone of a strong online presence. This involves ensuring it is user-friendly, mobile-responsive, and search engine optimized (SEO). SEO techniques, such as keyword research and on-page optimization, help your website rank higher in search engine results pages (SERPs). Additionally, a fast-loading website improves user experience and can positively impact your search engine rankings.  " +
-      "Consistent production of valuable and engaging content is essential for attracting and retaining an audience. This content can take various forms, including blog posts, articles, videos, infographics, and social media updates. " +
-      "The key is to create content that is relevant, informative, and shares your unique value proposition.  " +
-      "Social media platforms offer a powerful way to connect with your target audience and build relationships. By actively engaging with your followers, sharing valuable content, and participating in relevant conversations, you can increase your brand visibility and drive traffic to your website. It's important to select the right platforms for your audience and develop a consistent social media strategy.  " +
-      "Networking is crucial for building a strong online presence. Connect with other businesses, influencers, and industry experts through online communities, forums, and events. Collaborate on projects, share content, and offer support to build mutually beneficial relationships.  " +
-      "Email marketing remains a highly effective tool for reaching and engaging with your audience. Build an email list of subscribers and send regular, personalized newsletters with valuable content, updates, and promotions. Segment your email list to target specific audience segments with tailored messages.  " +
-      "For businesses with a local focus, local SEO is essential. Optimize your Google My Business listing with accurate information, photos, and reviews. Encourage customers to leave positive reviews and claim your business on other local directories.  " +
-      "To measure the effectiveness of your online presence, use analytics tools to track website traffic, user behavior, and social media engagement. Analyze the data to identify areas for improvement and make data-driven decisions to optimize your strategies.  " +
-      "High-quality visuals, such as images and videos, can enhance your content and make it more engaging. Use visually appealing elements to break up text and capture attention.  " +
-      "The digital landscape is constantly evolving. Stay informed about the latest trends, technologies, and best practices to ensure your online presence remains relevant and effective.  " +
-      "By implementing these strategies, you can build a strong online presence that helps you achieve your business goals and connect with your target audience. Remember, building a successful online presence is an ongoing process that requires consistent effort and adaptation."
   },]
 
 function Home() {
   return (
     <div className='home'>
       <div className="relative w-full h-screen overflow-hidden">
-        <video autoPlay muted loop className="absolute top-0 left-0 w-full h-full object-cover">
+        <video
+          autoPlay
+          muted
+          loop
+          playsInline
+          className="absolute top-0 left-0 w-full h-full object-cover"
+        >
           <source src="/assets/videos/video.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
